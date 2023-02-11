80dcebaf8b5eef3123a818c86778571fa6af8c05
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-02-10 21:35:13 -0300

    Carregando dados do produtor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -14,7 +15,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -29,13 +30,19 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider" >
       {products.map((product) => (
-        <Product key={product.id} className="keen-slider__slide" >
-          <Image src={product.imageUrl} width={520} height={480} alt=""/>
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+        <Link
+          key={product.id}
+          href={`/product/${product.id}`}
+          legacyBehavior
+        >
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt=""/>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
         ))}
     </HomeContainer>
   )
